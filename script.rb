8b2ab136b986cee4da1cc6d16ea39f35d7e19562
Alexandre Lopes <alexandre.lopes@enjoei.com.br>
2022-08-26 13:41:38 -0300

    add file

---
+++ script.rb
@@ -0,0 +1,14 @@
+require 'csv'
+
+filename = "#{Time.now.to_i}.csv"
+
+CSV.open(filename, "a+") do |csv|
+	csv << ["cluster_id", "priority", "product_id"]
+        (1000 * 1000).times do |i|
+                csv << ["test_cluster_ak", "123", "#{i}_product"]
+        end
+
+end
+
+puts "expires_at", Time.new(2022, 8, 18, 17, 30)
+puts "generated", filename
