6f5ae228fb35dc4f675dd76f68508b68390f1655
Alexandre Lopes <alexandre.lopes@enjoei.com.br>
2022-08-31 11:01:50 -0300

    add file

--- script.rb
+++ script.rb
@@ -6,7 +6,7 @@ timestamp = Time.now.to_i + 3600 # one hour ahead
 CSV.open(filename, "a+") do |csv|
 	csv << ["cluster_id", "priority", "product_id", "expires_at"]
         (700 * 1000).times do |i|
-                csv << ["test_cluster_bg", "123", "#{i}_product", timestamp.to_s]
+                csv << ["test_cluster_bh", "123", "#{i}_product", timestamp.to_s]
         end
 
 end
